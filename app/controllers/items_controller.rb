22f6005e0e80ef49624cabcea96b1e1c5c49a231
tamura3 <61034329+tamura3@users.noreply.github.com>
2020-04-13 22:36:56 +0900

    itemsコントローラー等作成

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,31 @@
+class ItemsController < ApplicationController
+
+  # 商品詳細ページ (田村)
+  def show
+  end
+
+  # トップページ・商品一覧ページ
+  def index
+  end
+
+  # 商品出品ページ
+  def new
+  end
+
+  # 商品出品機能
+  def create
+  end
+
+  # 商品情報更新ページ
+  def edit
+  end
+
+  # 商品情報更新機能
+  def update
+  end
+
+  # 商品削除機能
+  def destroy
+  end
+
+end
