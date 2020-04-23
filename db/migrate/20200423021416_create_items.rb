3334e6c3efbbfaf82296fda1943e3d8decd246ce
tamura3 <61034329+tamura3@users.noreply.github.com>
2020-04-23 12:10:32 +0900

    items マイグレーションファイルの作成

--- db/migrate/20200423021416_create_items.rb
+++ db/migrate/20200423021416_create_items.rb
@@ -1,7 +1,18 @@
 class CreateItems < ActiveRecord::Migration[5.2]
   def change
     create_table :items do |t|
-
+      t.string :name, null: false
+      t.integer :price, null: false
+      t.integer :status, null: false
+      t.string :brand, null: false
+      t.integer :shipping_fee, null:false
+      t.string :shipping_method, null:false
+      t.string :owners_area, null:false
+      t.integer :arrival_date, null:false
+      t.string :explain
+      t.references :category_id, foreign_key: true, null:false
+      t.references :user_id, foreign_key: true, null:false
+      t.integer :buyer_id
       t.timestamps
     end
   end
