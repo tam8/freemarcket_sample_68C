0e8a9c57db84a204a798b77be4ffe1576b03adf4
tam8 <22093763+tam8@users.noreply.github.com>
2020-04-24 16:19:31 +0900

    item_imageモデル

--- db/migrate/20200423022934_create_item_images.rb
+++ db/migrate/20200423022934_create_item_images.rb
@@ -1,7 +1,8 @@
 class CreateItemImages < ActiveRecord::Migration[5.2]
   def change
     create_table :item_images do |t|
-
+      t.string :image_url, null: false
+      t.references :item, foreign_key: true, null:false
       t.timestamps
     end
   end
