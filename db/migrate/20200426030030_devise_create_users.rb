cbd4d465b6e0ec78261f6c199095855290e57d21
shin-tmd <61884039+shin-tmd@users.noreply.github.com>
2020-04-26 19:54:56 +0900

    userテーブルを統一
    既存のuserテーブルをdeviseコマンドで作成されたテーブルにマージ

--- db/migrate/20200426030030_devise_create_users.rb
+++ db/migrate/20200426030030_devise_create_users.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
-class AddDeviseToUsers < ActiveRecord::Migration[5.2]
-  def self.up
-    change_table :users do |t|
+class DeviseCreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
       ## Database authenticatable
       t.string :email,              null: false, default: ""
+      t.string :nickname, null: false, unique: true
       t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
--- db/schema.rb
+++ db/schema.rb
@@ -44,10 +44,8 @@ ActiveRecord::Schema.define(version: 2020_04_26_030030) do
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "nickname", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.string "email", default: "", null: false
+    t.string "nickname", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
