c2e2b7c2319ad46395fe329e83131d6fd5354e03
aki2626 <akiyoshi.nagai.g@gmail.com>
2020-04-30 19:05:35 +0900

    item_status.rbの編集

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,12 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
+  belongs_to_active_hash :item_status
+
   belongs_to :user
   belongs_to :category
   # 不足っぽい
   has_many :item_images
   accepts_nested_attributes_for :item_images, allow_destroy: true  
+
 end
