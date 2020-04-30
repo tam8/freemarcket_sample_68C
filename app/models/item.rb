acdb47d5cbf65f0f70f153781675143ee296245c
aki2626 <akiyoshi.nagai.g@gmail.com>
2020-04-30 17:06:47 +0900

    itemsモデルへのprefectureモデルのアソシエーションの記載

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,6 @@ class Item < ApplicationRecord
   # 不足っぽい
   has_many :item_images
   accepts_nested_attributes_for :item_images, allow_destroy: true  
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
 end
