46fab6b3f70d818298f0d21f34018a77f81b2b4a
aki2626 <akiyoshi.nagai.g@gmail.com>
2020-05-01 15:00:36 +0900

    ite,_shipping_fee.rbの編集とitems#new.hamlへの反映

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,8 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :item_status
+  belongs_to_active_hash :item_shipping_fee
+
   belongs_to :user
   belongs_to :category
   # 不足っぽい
