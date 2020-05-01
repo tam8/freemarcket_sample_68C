e423177b586acbcb87d53c97f79d7392ec852da4
aki2626 <akiyoshi.nagai.g@gmail.com>
2020-05-01 15:29:40 +0900

    item_category.rbの編集とitems#newの反映

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,6 +5,7 @@ class Item < ApplicationRecord
   belongs_to_active_hash :item_shipping_fee
   belongs_to_active_hash :item_shipping_method
   belongs_to_active_hash :item_arrival_date
+  belongs_to_active_hash :item_category
 
   belongs_to :user
   belongs_to :category
