a8cab56425a4273e700fd461681b464ae704ed39
tamura3 <61034329+tamura3@users.noreply.github.com>
2020-05-04 18:36:07 +0900

    userテーブルの使用を再開

--- db/migrate/20200423021416_create_items.rb
+++ db/migrate/20200423021416_create_items.rb
@@ -11,7 +11,7 @@ class CreateItems < ActiveRecord::Migration[5.2]
       t.integer :arrival_date_id
       t.string  :explain
       t.integer :a_category_id
-      # t.integer :user_id, foreign_key: true, null:false
+      t.integer :user_id, foreign_key: true, null:false
       # user関係の作業が進んでから追加させる。その際reference型にさせるべきか 永井
       t.integer :buyer_id
       t.timestamps
