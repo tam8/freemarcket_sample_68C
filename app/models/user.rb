e3e110ba3ffb33750f6a1869355830193d309c15
shin-tmd <61884039+shin-tmd@users.noreply.github.com>
2020-05-04 23:57:25 +0900

    バリデーション追加、アドレステーブルとのアソシエーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :nickname, :first_name, :last_name, :age ,presence: true
   has_many :items
+  has_one :address
 end
