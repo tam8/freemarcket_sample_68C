f0246c7c95290733f060d7216042b1128b41350c
shin-tmd <61884039+shin-tmd@users.noreply.github.com>
2020-05-05 11:22:23 +0900

    住所情報登録ページへの遷移
    ユーザー情報のセッションへの保存およびaddressモデルのインスタンスを生成

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,6 +4,19 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(sign_up_params)
+    unless @user.valid?
+      flash.now[:alert] = @user.errors.full_messages
+      render :new and return
+    end
+    session["devise.regist_data"] = {user: @user.attributes}
+    session["devise.regist_data"][:user]["password"] = params[:user][:password]
+    @address = @user.build_address
+    render :new_address
+  end
+
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
