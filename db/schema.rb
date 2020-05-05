8007dc6aa5fcc00f1d95c14b89d32560f5ac2aa6
shin-tmd <61884039+shin-tmd@users.noreply.github.com>
2020-05-05 17:23:21 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -13,8 +13,12 @@
 ActiveRecord::Schema.define(version: 2020_05_04_113916) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "zipcode"
+    t.text "address"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_addresses_on_user_id"
   end
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
