dcea4bae0d2c32265b1ed3232877626129f6ca31
shin-tmd <61884039+shin-tmd@users.noreply.github.com>
2020-05-05 17:23:15 +0900

    アドレス登録画面への遷移および登録後のルーティング設定を記述

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/registrations',
   }
+  devise_scope :user do
+    get 'addresses', to: 'users/registrations#new_address'
+    post 'addresses', to: 'users/registrations#create_address'
+  end
   root 'items#index'
   resources :items do
 
