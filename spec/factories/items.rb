93d72d5d1bd2c528740c5aa1a56b3c1063d5a9bd
aki2626 <akiyoshi.nagai.g@gmail.com>
2020-05-08 14:35:25 +0900

    factory_botのitems.rbの編集
    tesuデータの記載

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    name                     {"test"}
+    price                    {"300"}
+    status_id                {"2"}
+    brand                    {"シャネル"}
+    shipping_fee_id          {"3"}
+    shipping_method_id       {"3"}
+    owners_area_id           {"3"}
+    arrival_date_id          {"2"}
+    explain                  {"これはいいテストです。"}
+    a_category_id            {"2"}
+    # a_categoryは後にcategoryに変更する
+    user_id                  {"1"}
+    buyer_id                 {"1"}
   end
 end
