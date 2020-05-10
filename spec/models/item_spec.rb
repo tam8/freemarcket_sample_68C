56cd13dd3d5019e8094277e64623b4f42788ed31
aki2626 <akiyoshi.nagai.g@gmail.com>
2020-05-10 13:02:18 +0900

    item_spec.rb　単体テストの更新２
    brandは空で、brand以外の全てのカラム情報が存在し、priceが300以上10000000未満の値が存在している時

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -11,7 +11,11 @@ describe Item do
     expect(item).to be_valid
   end
   
+  it "brandは空で、brand以外の全てのカラム情報が存在し、priceが300以上10000000未満の値が存在している時" do
+    item = build(:item, brand: nil)
+    expect(item).to be_valid
+  end
 
-  
+
   end
 end
