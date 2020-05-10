831e1762fa27a0963675907a14d2f272c4da1417
aki2626 <akiyoshi.nagai.g@gmail.com>
2020-05-10 13:00:05 +0900

    item_spec.rbの単体テストの更新　
    全てのカラム情報が存在し、priceが300以上10000000未満の値が存在している時

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -3,3 +3,15 @@ require 'rails_helper'
 RSpec.describe Item, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
 end
+
+describe Item do
+  describe '#create' do
+  it "全てのカラム情報が存在し、priceが300以上10000000未満の値が存在している時" do
+    item = build(:item)
+    expect(item).to be_valid
+  end
+  
+
+  
+  end
+end
