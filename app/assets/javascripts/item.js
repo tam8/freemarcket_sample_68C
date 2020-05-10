8f2962a34238dbc477e11896fe6c2891788cb2fc
tamura3 <61034329+tamura3@users.noreply.github.com>
2020-05-10 14:03:08 +0900

    永井さんの方、修正したら、これでも複数枚いけそう

--- app/assets/javascripts/item.js
+++ app/assets/javascripts/item.js
@@ -1,11 +1,11 @@
 $(function(){
 
-  let buiildFileField = (index) => {
+  let buildFileField = (index) => {
     
     const html = `<div class="js_file_group" data-index-id="${index}">
                     <input class="js_file_group__display" type="file" 
                     name="item[item_images_attributes][${index}][image_url]" 
-                    id="item_item_images_attributes_${index}_image_url">
+                    id="item_item_images_attributes_${index}_image">
                     <div class="js-remove">削除</div>
                   </div>`;
                 return html;
@@ -14,7 +14,7 @@ $(function(){
   let fileIndex = [1,2,3,4,5,6,7,8,9,10];
 
   $('.image_form').on('change', '.js_file_group__display', function(e) {
-    $('.image_form').append(buiildFileField(fileIndex[0]));
+    $('.image_form').append(buildFileField(fileIndex[0]));
     fileIndex.shift();
     // 末尾の数に1足した数を追加する
     fileIndex.push(fileIndex[fileIndex.length -1] +1)
