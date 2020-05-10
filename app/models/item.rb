b9489be7c824ae1264ce200e54868882df7585c7
aki2626 <akiyoshi.nagai.g@gmail.com>
2020-05-10 15:05:19 +0900

    item.rb　arrival_date_idのバリテーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -39,7 +39,11 @@ class Item < ApplicationRecord
   validates :owners_area_id      numericality: { only_integer: true,
                                                  greater_than_or_equal_to: 1,
                                                  less_than: 48}
+  validates :arrival_date_id     numericality: { only_integer: true,
+                                                  greater_than_or_equal_to: 1,
+                                                  less_than: 4}
 
+                                                  
   has_many :item_images
   accepts_nested_attributes_for :item_images, allow_destroy: true  
   validates :item_images,       presence: true
