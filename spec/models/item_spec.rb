9a64ef8a3faa2d23ddc2b25b7dcacd0f9b6713fa
aki2626 <akiyoshi.nagai.g@gmail.com>
2020-05-10 13:07:40 +0900

    item_spec.rbの更新4
    priceがない場合は登録できないこと

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -6,19 +6,26 @@ end
 
 describe Item do
   describe '#create' do
-  it "全てのカラム情報が存在し、priceが300以上10000000未満の値が存在している時" do
-    item = build(:item)
-    expect(item).to be_valid
-  end
-  
-  it "brandは空で、brand以外の全てのカラム情報が存在し、priceが300以上10000000未満の値が存在している時" do
-    item = build(:item, brand: nil)
-    expect(item).to be_valid
-  end
+    it "全てのカラム情報が存在し、priceが300以上10000000未満の値が存在している時" do
+      item = build(:item)
+      expect(item).to be_valid
+    end
+    
+    it "brandは空で、brand以外の全てのカラム情報が存在し、priceが300以上10000000未満の値が存在している時" do
+      item = build(:item, brand: nil)
+      expect(item).to be_valid
+    end
 
-  it "nameがないと場合は登録できないこと" do
-    item = build(:item, name: nil)
-    item.valid?
-    expect(item.errors[:name]).to include("can't be blank")
+    it "nameがない場合は登録できないこと" do
+      item = build(:item, name: nil)
+      item.valid?
+      expect(item.errors[:name]).to include("can't be blank")
+    end
+  
+    it "priceがない場合は登録できないこと" do
+      item = build(:item, price: nil)
+      item.valid?
+      expect(item.errors[:price]).to include("can't be blank")
+    end
   end
 end
