685feb51cb2d5fd43dbe6749bbc7f3ba85c213ff
hiro5631031001 <hiro5631031001@gmail.com>
2020-05-16 08:57:27 +0900

    seeds.rb にてカテゴリーの基礎データ入力

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_10_071935) do
+ActiveRecord::Schema.define(version: 2020_05_15_090627) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "ancestry"
+    t.index ["ancestry"], name: "index_categories_on_ancestry"
   end
 
   create_table "item_images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
