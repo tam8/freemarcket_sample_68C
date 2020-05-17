9e28cfff54654f8eb5ef48ae176868cfd7fa9414
shin-tmd <61884039+shin-tmd@users.noreply.github.com>
2020-05-17 19:54:09 +0900

    全角かな入力のみを許可設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :nickname, :first_name, :last_name, :birth_date, :first_name_kana, :last_name_kana ,presence: true
+  validates :first_name_kana, :last_name_kana ,presence: true,
+                                                format: {
+                                                  with: /\A[ぁ-んー－]+\z/,
+                                                  # allow_blank: true
+                                                  message: "全角ひらがなのみで入力して下さい"
+                                                }
+
+  devise :database_authenticatable, :validatable, password_length: 7..128
   has_many :items
   has_one :address
 end
