6b33c9139c31ce2e0d3bd7bffd1524256463a3f1
aki2626 <akiyoshi.nagai.g@gmail.com>
2020-05-17 19:42:12 +0900

    search.js　　inputによる動作の分岐の設定

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,4 +1,6 @@
 $(function(){
+  
+
   $('.head__in__up__search__btn--display').on("click", function(e){
     let input = $('.head__in__up__search__txt').val();
     e.preventDefault();
@@ -11,9 +13,18 @@ $(function(){
       .done(function(items) {
         $(".category-pickup__lists-box").empty();
         
+        if (items.length !== 0) {
+          items.forEach(function (item) {
+            addItem(item);
+          });
+        } else if (input.length == 0) {
+          return false;
+        } else {
+          addNoItem();
+        }
       })
       .fail(function() {
-        console.log("失敗です。");
+        alert("通信エラーです。商品が表示できません。")
       });
   });
 });
