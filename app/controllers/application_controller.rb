7571d7e784e83273f1386f6ab262fa6d9b83f88c
shin-tmd <61884039+shin-tmd@users.noreply.github.com>
2020-05-20 08:41:23 +0900

    不要なコメント削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   before_action :basic_auth, if: :production?
   protect_from_forgery with: :exception
 
-  # nicknameをカラムをpermit
   before_action :configure_permitted_parameters, if: :devise_controller?
   protected
   def configure_permitted_parameters
