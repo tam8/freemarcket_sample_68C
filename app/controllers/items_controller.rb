fb97b5be6ed6103b81cf1d9973b7e22a16916f39
hiro5631031001 <hiro5631031001@gmail.com>
2020-05-19 23:55:07 +0900

    items_controller 商品出品時のカテゴリー選択実装
    ストロングパラメーターのidをカラム変更に合わせて「a_category_id」から「category_id」へ変更
    ajax非同期通信、json使用のために、get_category_children,getcategory_grandchildrenを設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -17,13 +17,25 @@ class ItemsController < ApplicationController
 
   # 商品出品ページ
   def new
+
     if user_signed_in?
       @item = Item.new
       @item.item_images.new
+
+      @category_parent = Category.where(ancestry: nil)
+      
     else
       flash[:notice] = "商品の出品にはユーザー登録、もしくはログインをしてください"
       redirect_to new_user_registration_path
     end
+  end
+
+  def get_category_children
+    @category_children = Category.find(params[:parent_id]).children
+  end
+
+  def get_category_grandchildren
+    @category_grandchildren = Category.find(params[:child_id]).children
   end
 
   # 商品出品機能
@@ -91,7 +103,7 @@ class ItemsController < ApplicationController
                                  :owners_area_id, 
                                  :arrival_date_id, 
                                  :explain, 
-                                 :a_category_id, 
+                                 :category_id, 
                                  :buyer_id,
                                  item_images_attributes: [:image]
                                 #  :image_cache
@@ -119,7 +131,7 @@ class ItemsController < ApplicationController
       @arrival_date = @item.arrival_date.name
 
       # 仮
-      @category = @item.a_category.name
+      # @category = @item.category
       @user = User.find(@item.user_id).nickname
       @buyer = @item.buyer_id
 
