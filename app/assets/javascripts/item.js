1be9f2545dbe085b62baf9c9a3d87bcd5a5e581e
tam8 <22093763+tam8@users.noreply.github.com>
2020-05-21 11:40:24 +0900

    画像編集・削除できた

--- app/assets/javascripts/item.js
+++ app/assets/javascripts/item.js
@@ -12,6 +12,12 @@ $(document).on('turbolinks:load', ()=> {
 
   // file_fieldのnameに動的なindexをつける為の配列
   let fileIndex = [1,2,3,4,5,6,7,8,9,10];
+  // 既に使われているindexを除外 (新規追加するときののため)
+  lastIndex = $('.js-file_group:last').data('index');
+  fileIndex.splice(0, lastIndex);
+  // 削除用チェックボックスを非表示
+  $('.hidden-destroy').hide();
+
 
   $('.image_form').on('change', '.js_file_group__display', function(e) {
     // fileIndexの先頭の数字を使ってinputを作る
@@ -22,6 +28,11 @@ $(document).on('turbolinks:load', ()=> {
   });
 
   $('.image_form').on('click', '.js-remove', function() {
+    const targetIndex = $(this).parent().data('index')
+    // 該当indexを振られているチェックボックスを取得する
+    const hiddenCheck = $(`input[data-index="${targetIndex}"].hidden-destroy`);
+    // もしチェックボックスが存在すればチェックを入れる
+    if (hiddenCheck) hiddenCheck.prop('checked', true);
     $(this).parent().remove();
     // 画像入力欄が0個にならないようにしておく
     if ($('.js_file_group__display').length == 0) $('.image_form').append(buildFileField(fileIndex[0]));
