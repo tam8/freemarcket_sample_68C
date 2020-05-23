abdd017e5092df9a638d511e0534cd0a76b61f1a
hiro5631031001 <hiro5631031001@gmail.com>
2020-05-23 11:57:56 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2020_05_04_113916) do
+ActiveRecord::Schema.define(version: 2020_05_19_132417) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "zipcode", null: false
@@ -25,9 +24,6 @@ ActiveRecord::Schema.define(version: 2020_05_04_113916) do
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_addresses_on_user_id"
   end
-
-ActiveRecord::Schema.define(version: 2020_05_10_071935) do
-
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
