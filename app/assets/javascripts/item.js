da15044dbbb5e5cd48508460b87216cff2d2a524
tam8 <22093763+tam8@users.noreply.github.com>
2020-05-24 15:08:41 +0900

    整理した (まだ動かない)

--- app/assets/javascripts/item.js
+++ app/assets/javascripts/item.js
@@ -1,5 +1,7 @@
 $(document).on('turbolinks:load', ()=> {
-  //タグ生成用関数
+
+  //タグ生成用関数 /////////////////////////////////////////
+
     // 画像用のinputを生成する関数
     const buildFileField = (index)=> {
       const html = `<div data-index="${index}" class="js_file_group">
@@ -17,7 +19,9 @@ $(document).on('turbolinks:load', ()=> {
       return html;
     }
 
-  // 変数定義
+
+  // 変数定義 ////////////////////////////////////////////////
+ 
     // file_fieldのnameに動的なindexをつける為の配列
     let fileIndex = [1,2,3,4,5,6,7,8,9,10];
 
@@ -26,52 +30,52 @@ $(document).on('turbolinks:load', ()=> {
     fileIndex.splice(0, lastIndex);
 
 
-  // 削除用チェックボックスを非表示
-  $('.hidden-destroy').hide();
+  // 処理部分 ///////////////////////////////////////////////
+
+    // 削除用チェックボックスを非表示
+    $('.hidden-destroy').hide();
 
 
-  // 処理部分
-    //画像入稿部分で変更があった際
-      $('.image_form').on('change', '.js_file_group__display', function(e) {
+    // 変更があった際
+    $('.image_form').on('change', '.js_file_group__display', function(e) {
 
-        // ターゲットとなるindexを取得
-        const targetIndex = $(this).parent().data('index');
-        const file = e.target.files[0];
+      // ターゲットとなるindexを取得
+      const targetIndex = $(this).parent().data('index');
+      const file = e.target.files[0];
 
-        const blobUrl = window.URL.createObjectURL(file);
+      const blobUrl = window.URL.createObjectURL(file);
 
-        // 該当indexのimgがあれば変数imgに入れる(画像変更の処理)
-        if (img = $(`img[data-index="${targetIndex}"]`)[0]) {
-          img.setAttribute('src', blobUrl);
-        } else {  // 新規画像追加の処理
-          $('.image_preview').append(buildImg(targetIndex, blobUrl));
+      // 該当indexのimgがあれば変数imgに入れる(画像変更の処理)
+      if (img = $(`img[data-index="${targetIndex}"]`)[0]) {
+        img.setAttribute('src', blobUrl);
+      } else {  // 新規画像追加の処理
+        $('.image_preview').append(buildImg(targetIndex, blobUrl));
 
-        // fileIndexの先頭の数でinputを生成
-        $('.image_form').append(buildFileField(fileIndex[0]));
-        fileIndex.shift();
+      // fileIndexの先頭の数でinputを生成
+      $('.image_form').append(buildFileField(fileIndex[0]));
+      fileIndex.shift();
 
-        // 末尾に1足した数を追加
-        fileIndex.push(fileIndex[fileIndex.length - 1] + 1)
-      });
+      // 末尾に1足した数を追加
+      fileIndex.push(fileIndex[fileIndex.length - 1] + 1)
+    });
 
 
     // 削除ボタンを押した際
       $('.image_form').on('click', '.js-remove', function() {
 
         // ターゲットとなるindexを取得
-        const targetIndex = $(this).parent().data('index')
+        const targetIndex = $(this).parent().data('index');
         const hiddenCheck = $(`input[data-index="${targetIndex}"].hidden-destroy`);
 
         // もしチェックボックスが存在すればチェックを入れる
         if (hiddenCheck) hiddenCheck.prop('checked', true);
 
         $(this).parent().remove();
+        // プレビュー画像の削除
+        $(`img[data-index="${targetIndex}"]`).remove();
 
         // 入力欄が0個にならないように
         if ($('.js_file_group__display').length == 0) $('.image_form').append(buildFileField(fileIndex[0]));
-
-        // プレビュー画像の削除
-        $(`img[data-index="${targetIndex}"]`).remove();
 
       });
 
