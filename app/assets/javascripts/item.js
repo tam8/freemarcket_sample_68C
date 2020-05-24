5b238aaa60f52c7aa911efbebce36221d91ef7c0
tam8 <22093763+tam8@users.noreply.github.com>
2020-05-24 14:38:05 +0900

    一旦  退避 (全体の記述は終わった)

--- app/assets/javascripts/item.js
+++ app/assets/javascripts/item.js
@@ -1,74 +1,81 @@
 $(document).on('turbolinks:load', ()=> {
-  // 画像用のinputを生成する関数
-  const buildFileField = (index)=> {
-    const html = `<div data-index="${index}" class="js_file_group">
-                    <i class="fas fa-minus-circle js-remove"></i>
-                    <input class="js_file_group__display" type="file"
-                    name="item[item_images_attributes][${index}][image]"
-                    id="item_item_images_attributes_${index}_image">
-                  </div>`;
-    return html;
-  }
+  //タグ生成用関数
+    // 画像用のinputを生成する関数
+    const buildFileField = (index)=> {
+      const html = `<div data-index="${index}" class="js_file_group">
+                      <i class="fas fa-minus-circle js-remove"></i>
+                      <input class="js_file_group__display" type="file"
+                      name="item[item_images_attributes][${index}][image]"
+                      id="item_item_images_attributes_${index}_image">
+                    </div>`;
+      return html;
+    }
 
-  // プレビュー用のimgタグを生成する関数
-  const buildImg = (index, url)=> {
-    const html = `<img data-index="${index}" src="${url}" width="100px" height="100px">`;
-    return html;
-  }
+    // プレビューimgタグを生成する関数
+    const buildImg = (index, url)=> {
+      const html = `<img data-index="${index}" src="${url}" width="100px" height="100px">`;
+      return html;
+    }
 
-  // file_fieldのnameに動的なindexをつける為の配列
-  let fileIndex = [1,2,3,4,5,6,7,8,9,10];
-  // 既に使われているindexを除外 (新規追加するときののため)
-  lastIndex = $('.js-file_group:last').data('index');
-  fileIndex.splice(0, lastIndex);
+  // 変数定義
+    // file_fieldのnameに動的なindexをつける為の配列
+    let fileIndex = [1,2,3,4,5,6,7,8,9,10];
+
+    // 使用済みのindexを削除 (新規追加のため)
+    lastIndex = $('.js-file_group:last').data('index');
+    fileIndex.splice(0, lastIndex);
+
+
   // 削除用チェックボックスを非表示
   $('.hidden-destroy').hide();
 
 
-  $('.image_form').on('change', '.js_file_group__display', function(e) {
-    // fileIndexの先頭の数字を使ってinputを作る
-    $('.image_form').append(buildFileField(fileIndex[0]));
-    fileIndex.shift();
-    // 末尾の数に1足した数を追加する
-    fileIndex.push(fileIndex[fileIndex.length - 1] + 1)
-  });
+  // 処理部分
+    //画像入稿部分で変更があった際
+      $('.image_form').on('change', '.js_file_group__display', function(e) {
 
-  $('.image_form').on('click', '.js-remove', function() {
-    const targetIndex = $(this).parent().data('index')
-    // 該当indexを振られているチェックボックスを取得する
-    const hiddenCheck = $(`input[data-index="${targetIndex}"].hidden-destroy`);
-    // もしチェックボックスが存在すればチェックを入れる
-    if (hiddenCheck) hiddenCheck.prop('checked', true);
-    $(this).parent().remove();
-    // 画像入力欄が0個にならないようにしておく
-    if ($('.js_file_group__display').length == 0) $('.image_form').append(buildFileField(fileIndex[0]));
-  });
+        // ターゲットとなるindexを取得
+        const targetIndex = $(this).parent().data('index');
+        const file = e.target.files[0];
+
+        const blobUrl = window.URL.createObjectURL(file);
+
+        // 該当indexのimgがあれば変数imgに入れる(画像変更の処理)
+        if (img = $(`img[data-index="${targetIndex}"]`)[0]) {
+          img.setAttribute('src', blobUrl);
+        } else {  // 新規画像追加の処理
+          $('.image_preview').append(buildImg(targetIndex, blobUrl));
+
+        // fileIndexの先頭の数でinputを生成
+        $('.image_form').append(buildFileField(fileIndex[0]));
+        fileIndex.shift();
+
+        // 末尾に1足した数を追加
+        fileIndex.push(fileIndex[fileIndex.length - 1] + 1)
+      });
+
+
+    // 削除ボタンを押した際
+      $('.image_form').on('click', '.js-remove', function() {
+
+        // ターゲットとなるindexを取得
+        const targetIndex = $(this).parent().data('index')
+        const hiddenCheck = $(`input[data-index="${targetIndex}"].hidden-destroy`);
+
+        // もしチェックボックスが存在すればチェックを入れる
+        if (hiddenCheck) hiddenCheck.prop('checked', true);
+
+        $(this).parent().remove();
+
+        // 入力欄が0個にならないように
+        if ($('.js_file_group__display').length == 0) $('.image_form').append(buildFileField(fileIndex[0]));
+
+        // プレビュー画像の削除
+        $(`img[data-index="${targetIndex}"]`).remove();
+
+      });
+
 });
 
 
 
-
-
-// 永井さんのやつ修正済み
-// $(function(){
-
-//   let buildFileField = (index) => {
-    
-//     const html = `<div class="js_file_group" data-index="${index}">
-//                     <input class="js_file_group__display" type="file" 
-//                     name="item[item_images_attributes][${index}][image]" 
-//                     id="item_item_images_attributes_${index}_image">
-//                     <div class="js-remove">削除</div>
-//                   </div>`;
-//                 return html;
-//   }
-
-//   let fileIndex = [1,2,3,4,5,6,7,8,9,10];
-
-//   $('.image_form').on('change', '.js_file_group__display', function(e) {
-//     $('.image_form').append(buildFileField(fileIndex[0]));
-//     fileIndex.shift();
-//     // 末尾の数に1足した数を追加する
-//     fileIndex.push(fileIndex[fileIndex.length -1] +1)
-//   });
-// })
