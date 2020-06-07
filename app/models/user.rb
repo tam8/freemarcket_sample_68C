e5652f07ced27aaa8347cb4fc4b38127dd9a4a53
aki2626 <akiyoshi.nagai.g@gmail.com>
2020-06-07 15:58:09 +0900

    userモデルの編集
    omnuauthを使うためのオプションとアソシエーションの追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
   validates :nickname, :first_name, :last_name, :birth_date, :first_name_kana, :last_name_kana ,presence: true
   validates :first_name_kana, :last_name_kana ,presence: true,
                                                 format: {
@@ -12,4 +12,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :validatable, password_length: 7..128
   has_many :items
   has_one :address
+  has_many :sns_credentials
 end
