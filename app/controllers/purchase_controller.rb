3d6d0eb1d9aa8280e4282ed2911059a89c5c200f
shin-tmd <61884039+shin-tmd@users.noreply.github.com>
2020-06-18 15:44:25 +0900

    修正点の改修3

--- app/controllers/purchase_controller.rb
+++ app/controllers/purchase_controller.rb
@@ -26,9 +26,9 @@ class PurchaseController < ApplicationController
     card = Card.where(user_id: current_user.id).first
     Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
     Payjp::Charge.create(
-    :amount => @item.price, #支払金額を入力（itemテーブル紐づけ）
-    :customer => card.customer_id, #顧客ID
-    :currency => 'jpy', #日本円
+    amount: @item.price, #支払金額を入力（itemテーブル紐づけ）
+    customer: card.customer_id, #顧客ID
+    currency: 'jpy', #日本円
   )
   if @item.update(buyer_id: current_user.id)
     redirect_to action: 'done' #完了画面に移動
