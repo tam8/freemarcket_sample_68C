c5b95bfb3f95b400bcbd455714f75013f7e79e54
tam8 <22093763+tam8@users.noreply.github.com>
2020-06-22 02:47:50 +0000

    currentへcd

--- sh/reset_pro.sh
+++ sh/reset_pro.sh
@@ -1,1 +1,4 @@
-rails db:migrate:reset RAILS_ENV=production DISABLE_DATABASE_ENVIRONMENT_CHECK=1 && rails db:seed RAILS_ENV=production DISABLE_DATABASE_ENVIRONMENT_CHECK=1
+cd current
+rails db:migrate:reset RAILS_ENV=production DISABLE_DATABASE_ENVIRONMENT_CHECK=1
+cd current
+rails db:seed RAILS_ENV=production DISABLE_DATABASE_ENVIRONMENT_CHECK=1
