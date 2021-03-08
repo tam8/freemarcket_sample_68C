ebb01fc0b6dcbf7760de9c3d85f7e9ae72a0e39a
tam8 <22093763+tam8@users.noreply.github.com>
2021-03-09 02:52:11 +0900

    Heroku用にPumaへ変更

--- config/puma.rb
+++ config/puma.rb
@@ -4,16 +4,19 @@
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
 # and maximum; this matches the default thread size of Active Record.
 #
-threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
+
+workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
 threads threads_count, threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
-port        ENV.fetch("PORT") { 3000 }
+rackup      DefaultRackup
+port        ENV['PORT']     || 3000
 
 # Specifies the `environment` that Puma will run in.
 #
-environment ENV.fetch("RAILS_ENV") { "development" }
+environment ENV['RACK_ENV'] || 'development'
 
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked webserver processes. If using threads and workers together
@@ -23,12 +26,14 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 #
 # workers ENV.fetch("WEB_CONCURRENCY") { 2 }
 
+
+
 # Use the `preload_app!` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code
 # before forking the application. This takes advantage of Copy On Write
 # process behavior so workers use less memory.
 #
-# preload_app!
+preload_app!
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
@@ -36,3 +41,9 @@ plugin :tmp_restart
 # cert = "/home/ec2-user/freemarcket_sample_68C/ssl/fullchain.pem"
 # key = "/home/ec2-user/freemarcket_sample_68C/ssl/key.pem"
 # ssl_bind "0.0.0.0", 3000, cert: cert, key: key
+
+on_worker_boot do
+  # Worker specific setup for Rails 4.1+
+  # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
+  ActiveRecord::Base.establish_connection
+end
